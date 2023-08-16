e9593edf5252c060d4ebc13110afec40b1d18107
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-08-16 21:47:48 +0500

    [FEAT] Added basic server setup :muscle:

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello, World!')
+import dotenv from 'dotenv';
+dotenv.config();
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(3000, () => console.log(`Server running on http://localhost:${PORT}/`));
