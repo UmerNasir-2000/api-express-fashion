d42a170701bb66791d90f2308d0651611811e971
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-08-17 22:43:19 +0500

    [CHORE] Cleaned up project 🚸

--- app/src/modules/product/product.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import ProductService from './product.service';
-
-const router = Router();
-
-router.route('/')
-      .get(async (_, res) => res.status(200).json(await ProductService.listProducts()));
-
-export default router;
