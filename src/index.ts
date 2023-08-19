25cd700af4a35ed0be3ee14590fe7759820be92b
umer-maqsad <122520233+umer-maqsad@users.noreply.github.com>
2023-08-19 21:34:18 +0500

    [FEAT] Abstracted status codes ♻️

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
+import { StatusCodes } from 'http-status-codes';
+
 import Logger from './lib/logger';
 import setupRoutes from './routes';
 
@@ -9,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => res.status(200).json({ status: 'Alive' }));
+app.get('/', (req, res) => res.status(StatusCodes.OK).json({ status: 'Alive' }));
 
 const PORT = process.env.PORT || 5000;
 
--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,11 @@
 
 import { Router } from 'express';
+import { StatusCodes } from 'http-status-codes';
+
 import ProductService from '../services';
 
 const router = Router();
 
-router.get('/', async (req, res) => res.status(200).json(await ProductService.listProduct())) 
+router.get('/', async (req, res) => res.status(StatusCodes.OK).json(await ProductService.listProduct())) 
       
 export default router;
